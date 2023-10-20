905d934a7928ecab49bd16a4f55568cf28344ddb
MadhavWalia <madhavwalia8139@gmail.com>
2023-10-20 13:08:21 +0530

    Formatted the code

--- backend/Client/middleware/Networks.py
+++ backend/Client/middleware/Networks.py
@@ -1,3 +1,4 @@
+from typing import Any
 import numpy as np
 
 
@@ -31,7 +32,7 @@ def MSE_P(y, y_hat):
     return 2 * (y_hat - y) / y.size
 
 
-"""Networks"""
+"""Network"""
 
 
 class Layer:
@@ -60,14 +61,8 @@ class FullyConnected(Layer):
         precision=10**4,
     ) -> None:
         super().__init__(precision)
-        weights = np.random.randint(
-            -self.precision,
-            self.precision,
-            size=(self.output_dimension, self.input_dimension),
-        )
-        bias = np.random.randint(
-            -self.precision, self.precision, size=(self.output_dimension,)
-        )
+        self.weights = None
+        self.biases = None
         self.inputSize = input_size
         self.outputSize = output_size
         self.activation = activation
@@ -84,18 +79,21 @@ class FullyConnected(Layer):
         self.biases -= lr * error
         return error
 
-    def setwights(self, weights):
+    def set_wights(self, weights):
         self.weights = np.array(weights)
         self.weights = self.weights.T
 
-    def setbiases(self, biases):
+    def set_biases(self, biases):
         self.biases = np.array(biases).reshape(1, -1)
 
-    def getweights(self):
+    def get_weights(self):
         return self.weights.T
 
-    def getbiases(self):
+    def get_biases(self):
         return self.biases.T
+
+    def set_precision(self, precision):
+        self.precision = precision
 
 
 class ANN:
@@ -120,3 +118,65 @@ class ANN:
             layer.set_weights(weights)
             layer.set_bias(bias)
         self.layers.append(layer)
+
+    def set_loss(self, loss, loss_p):
+        self.loss = loss
+        self.loss_p = loss_p
+
+    def set_weights(self, weights):
+        for layer in self.layers:
+            if isinstance(layer, FullyConnected):
+                layer.set_weights(weights)
+
+    def set_biases(self, biases):
+        for layer in self.layers:
+            if isinstance(layer, FullyConnected):
+                layer.set_biases(biases)
+
+    def get_weights(self):
+        for layer in self.layers:
+            if isinstance(layer, FullyConnected):
+                return layer.get_weights()
+
+    def get_biases(self):
+        for layer in self.layers:
+            if isinstance(layer, FullyConnected):
+                return layer.get_biases()
+
+    def set_precision(self, precision):
+        self.precision = precision
+        for layer in self.layers:
+            if isinstance(layer, FullyConnected):
+                return layer.set_precision(precision)
+
+    def predict(self, input_data):
+        samples = len(input_data)
+        input_data = input_data * self.precision
+        input_data = input_data.astype(int)
+        result = []
+
+        for i in range(samples):
+            output = input_data[i]
+            for layer in self.layers:
+                output = layer.forward(output)
+            output = np.argmax(output) + 1
+            result.append(output)
+        return result
+
+    def fit(self, x_train, y_train, epochs, learning_rate):
+        samples = len(x_train)
+        for i in range(epochs):
+            err = 0
+            for j in range(samples):
+                output = x_train[j] * self.precision
+                output = output.astype(int)
+                y_true = np.zeros(self.output_dimension)
+                y_true[y_train[j] - 1] = self.precision
+                for layer in self.layers:
+                    output = layer.forward_propagation(output)
+                err += self.loss(y_true, output, precision=self.precision)
+                error = self.loss_prime(y_true, output).astype(int)
+                for layer in reversed(self.layers):
+                    error = layer.backward_propagation(error, learning_rate)
+
+            err /= samples
